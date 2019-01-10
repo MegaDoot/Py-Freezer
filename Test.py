14640416a1494c5c862ca1aef7f80b89208157e1
Alex <16scorzaa@cedarsupper.co.uk>
2019-01-10 22:29:55 +0000

    Minor Modifications

--- Test.py
+++ Test.py
@@ -1,6 +1,8 @@
 import tkinter as tk
+import time
 
 root = tk.Tk()
 root.title("It works!")
-tk.Label(text = "Foobar").grid()
+tk.Label(root, text = "Foobar").pack()
 root.mainloop()
+time.sleep(10)
