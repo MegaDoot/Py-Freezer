abf644efae737165211b347d4169a13998dc7ab2
MadLad <41120880+MegaDoot@users.noreply.github.com>
2019-01-12 12:51:41 +0000

    Added regex - build files from anywhere

--- Setup.py
+++ Setup.py
@@ -1,19 +1,19 @@
 """
 Edited and adapted from:
 https://pythonprogramming.net/converting-python-scripts-exe-executables/
-
-This program is intended to be run on the command line with one argument
 """
 
 
 from cx_Freeze import setup, Executable
 import os
+import re
 
 os.environ["TCL_LIBRARY"] = os.environ["TK_LIBRARY"] = r"C:\Users\alexs\Desktop\Non-Game Applications\Coding\Cmder\vendor\git-for-windows\mingw64\lib\tcl8.6"
 
-filename = open("Filename.txt")
+filename = open("Filename.txt").read()
+name = re.compile(r"[A-Za-z]+\.py(w?)$").search(filename).group() #Get name of file only
 
-setup(name = "Test",
+setup(name = os.path.splitext(filename)[0],
       version = "0.1" ,
       description = "" ,
-      executables = [Executable("Test.py")])
+      executables = [Executable(filename)])
