6137278241bc281ef0ab41f443a7cb5804e7dcbe
Alex <16scorzaa@cedarsupper.co.uk>
2019-01-12 17:52:01 +0000

    Works in any directory, button disables correctly
    Also added some comments

--- I never freeze.py
+++ I never freeze.py
@@ -1,7 +1,10 @@
 r"""
 Made by Alex Scorza
-Using Cx_freeze and tkfilebrowser, which are automatically installed
-(Good thing all of these computers have pip)
+pip and python are needed. Either set them as path variables, the setx command
+or navigate to where python.exe and pip.exe are installed and run this program
+from the command line.
+
+Cx_freeze and tkfilebrowser are automatically installed, as it pip 18.2
 github repo = https://github.com/MegaDoot/Py-Freezer
 """
 
@@ -16,7 +19,6 @@ def install_lib(name_pip, name_imp):
         print("Installing " + name_imp)
     exec("import " + name_imp)
 
-##os.chdir("C:\\Program Files\\Python36")
 if float(pip.__version__) < 18.1:
     os.system("python -m pip install --upgrade pip")
 
@@ -45,7 +47,8 @@ btn_style = {
     "overrelief":"solid",
     "bg":bg,
     "font":(font, 12),
-    "activebackground":"#f4b43e"
+    "activebackground":"#f4b43e",
+    "disabledforeground":"#848484",
     }
 
 entry_style = {
@@ -58,64 +61,46 @@ entry_style = {
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.title(".py freezer")
+        self.title(".py/.pyw freezer")
         self.configure(bg = bg) #Set background of root to normal background
 
         ttk_style = ttk.Style() #For ttk widgets (can't use dictionary)
         ttk_style.configure("BW.TLabel", background = "white", foreground = fg, relief = "solid") #Set style to dictionary 'entry_style'
 
         self.sv_inputs = tuple(tk.StringVar(self, value = cwd) for x in range(2))
+        self.sv_file = tk.StringVar(self, value = no_selection)
+        self.sv_constrain_io = tk.BooleanVar(self, value = True)
         
         tk.Label(text = "PY To EXE Converter", font = (font, 25), bg = bg, **style).grid(row = 0, column = 1)
-
-        self.sv_constrain_io = tk.BooleanVar(self, value = True)
         tk.Checkbutton(self, text = "Input Dir = Output Dir", variable = self.sv_constrain_io, **style, activebackground = bg, bg = bg, font = (font, 15)).grid(row = 2, column = 1)
-        
+
         self.entries = [tk.Entry(self, **style, **entry_style, textvariable = self.sv_inputs[0]) for x in range(2)]
         for i in range(2):
             row = 1 + (2 * i)
             self.entries[i].grid(row = row, column = 1, sticky = "NS")
             tk.Button(self, text = "Browse", **btn_style, **style, command = lambda i = i: self.set_filename(i)).grid(row = row, column = 2, sticky = "NS", padx = 5)
             tk.Label(self, text = ("Input:", "Output:")[i], font = (font, 15), bg = bg, **style).grid(row = row, column = 0, padx = 5, sticky = "E")
-            self.sv_inputs[i].trace("w", lambda n, m,x: self.dir_trace())
-        tk.Label(self, text = "Select A py/pyw File In Input Directory:", font = (font, 15), bg = bg, **style).grid(row = 4, column = 1)
+            self.sv_inputs[i].trace("w", lambda n, m, x: self.dir_trace())
+        self.sv_constrain_io.trace("w", self.constraint_trace)
 
-        self.sv_file = tk.StringVar(self, value = no_selection)
-        
+        tk.Label(self, text = "Select A py/pyw File In Input Directory:", font = (font, 15), bg = bg, **style).grid(row = 4, column = 1)
         self.in_dir_cb = ttk.Combobox(self, textvariable = self.sv_file, style = "BW.TLabel", width = 20, font = (font, 15), state = "readonly")
         self.in_dir_cb.grid(row = 5, column = 1)
-
-        self.build_btn = tk.Button(text = "Freeze/Build", **style, **btn_style, command = self.build)
+        self.build_btn = tk.Button(text = "Freeze/Build", **style, **btn_style, command = self.build, state = "disabled")
         self.build_btn.grid(row = 6, column = 1, pady = 5)
         
-        self.sv_constrain_io.trace("w", self.constraint_trace)
-##
-##        self.stacks = [self.sv_inputs[i].get() for i in range(2)]
-##        self.stack_positions = [0] * 2
-
         self.bind("<Button-1>", self.flatten)
-        self.bind("<<ComboboxSelected>>", lambda event: self.in_dir_cb.selection_clear())
-##        self.bind("<Control-Z>", self.undo)
+        self.bind("<<ComboboxSelected>>", lambda event: [self.in_dir_cb.selection_clear(), self.build_btn.config(state = "normal")])
+
         self.constraint_trace()
         self.dir_trace()
-
-##    def new_change(self, which):
-##        print(self.stacks)
-##        if len(self.stacks[i]) >= 10:
-##            del self.stacks[i][0]
-##        self.stacks[which].append(self.sv_inputs[which].get())
-##
-##    def undo(self, which):
-##        if self.stack_positions[which] > 0:
-##            self.stack_positions[which] -= 1
-##        self.set_entry(which, self.stacks[which])
-
+        
     def build(self):
         os.chdir(cwd)
         with open("Filename.txt", "w") as file:
             file.write(os.path.realpath(self.sv_file.get()))
         os.chdir(self.sv_inputs[not self.sv_constrain_io.get()].get()) #Navigate to output directory
-##        os.system('python "{}\\Setup.py" build'.format(cwd)) #Regardless of current directory
+        os.system('python "{}/Setup.py" build & Pause'.format(cwd)) #Regardless of current directory
 
     def flatten(self, event):
         """By default, buttons become sunken when clicked. This is called whenever a
@@ -128,13 +113,16 @@ class App(tk.Tk):
 
     def dir_trace(self, *args):
         """Called whenever entries updated"""
-        self.sv_inputs[0].set(self.sv_inputs[0].get())
+        self.build_btn.config(state = "disabled")
+        for i in range(2):
+            self.sv_inputs[i].set(self.sv_inputs[i].get().replace("/", "\\"))
         if os.path.isdir(self.sv_inputs[0].get()):
             os.chdir(self.sv_inputs[0].get())
             self.in_dir_cb.config(values = [file for file in os.listdir() if os.path.splitext(file)[1] in (".py", ".pyw")])
             self.sv_file.set(no_selection)
         else:
             self.sv_file.set(invalid_dir)
+            self.in_dir_cb.config(values = [])
 
     def constraint_trace(self, *args):
         """When checkbutton changed, the second textbox will either share a textvariable
